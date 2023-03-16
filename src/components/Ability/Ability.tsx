c79df7eb34a83c2f4e21870f590b932b42a5ad1d
mathiasck <mathias.ck2000@gmail.com>
2023-03-16 22:02:42 +0100

    Fetch ability data

--- src/components/Ability/Ability.tsx
+++ src/components/Ability/Ability.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {NameUrlPair} from "../../types";
 
 interface Props {
@@ -6,7 +6,26 @@ interface Props {
 }
 
 const Ability: React.FC<Props> = ({ability}) => {
-  return <div>{ability.name}</div>;
+  const [abilityData, setAbilityData] = useState<any>({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(ability.url);
+      const data = await response.json();
+      setAbilityData(data);
+    };
+
+    fetchData();
+  }, [ability.url]);
+  if (!abilityData) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <h2>{ability.name}</h2>
+      <p>{abilityData.effect_entries[0].effect}</p>
+    </div>
+  );
 };
 
 export default Ability;
