4fa88ee953c9f83b392f007c11c4dfb8d793f67b
mathiasck <mathias.ck2000@gmail.com>
2023-03-16 20:56:52 +0100

    Add Header component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Pokemon} from "../../types";
 import styles from "./Card.module.css";
+import Header from "../Header";
 
 interface Props {
   pokemon?: Pokemon;
@@ -10,11 +11,10 @@ const Card: React.FunctionComponent<Props> = ({pokemon}) => {
   if (!pokemon) {
     return <div>"Loading..."</div>;
   }
-  debugger;
+  console.log(pokemon);
   return (
     <div className={styles.infoContainer}>
-      <h1>Name: {pokemon.name}</h1>
-      <h1>Height: {pokemon.height}</h1>
+      <Header name={pokemon.name} height={pokemon.height} />
       <img
         src={pokemon.sprites.other.dream_world.front_default}
         alt={`${pokemon.name} illustration`}
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const Header: React.FunctionComponent = () => {
-  return <div>Header</div>;
+interface Props {
+  name: string;
+  height: number;
+}
+
+const Header: React.FunctionComponent<Props> = ({name, height}) => {
+  return (
+    <div>
+      <h1>Name: {name}</h1>
+      <h1>Height: {height}</h1>
+    </div>
+  );
 };
 
 export default Header;
