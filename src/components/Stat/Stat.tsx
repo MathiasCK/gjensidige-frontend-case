3bcb5dba257cd18307f4b92045ce04b69f5d7582
mathiasck <mathias.ck2000@gmail.com>
2023-03-16 22:10:08 +0100

    Fetch stats data

--- src/components/Stat/Stat.tsx
+++ src/components/Stat/Stat.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {NameUrlPair} from "../../types";
 
 interface Props {
@@ -6,7 +6,28 @@ interface Props {
 }
 
 const Stat: React.FC<Props> = ({stat}) => {
-  return <div>{stat.name}</div>;
+  const [statData, setStatData] = useState<any>({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(stat.url);
+      const data = await response.json();
+      setStatData(data);
+    };
+
+    fetchData();
+  }, [stat.url]);
+
+  if (!statData) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <h2>
+        {statData.name} : {statData.game_index}
+      </h2>
+    </div>
+  );
 };
 
 export default Stat;
