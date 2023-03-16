9178d10d9219013ff55ea16051939a7c7cb0004e
mathiasck <mathias.ck2000@gmail.com>
2023-03-16 22:14:21 +0100

    Fetch moves data

--- src/components/Move/Move.tsx
+++ src/components/Move/Move.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {NameUrlPair} from "../../types";
 
 interface Props {
@@ -6,7 +6,28 @@ interface Props {
 }
 
 const Move: React.FC<Props> = ({move}) => {
-  return <div>{move.name}</div>;
+  const [moveData, setMoveData] = useState<any>({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(move.url);
+      const data = await response.json();
+      setMoveData(data);
+    };
+
+    fetchData();
+  }, [move.url]);
+
+  if (!moveData) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <h2>{moveData?.name}</h2>
+      <h3>Power: {moveData?.power}</h3>
+      <h3>Type: {moveData?.type?.name}</h3>
+    </div>
+  );
 };
 
 export default Move;
