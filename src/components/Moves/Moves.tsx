89fef53bc9b9627f1ed14c47c15c60bf4a9450db
mathiasck <mathias.ck2000@gmail.com>
2023-03-17 11:35:47 +0100

    Moves: css cleanup

--- src/components/Moves/Moves.tsx
+++ src/components/Moves/Moves.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Move from "../Move";
 import {Move as IMove} from "../../types";
-import styles from "./Moves.module.css";
+import "./moves.scss";
 import {useSelector} from "react-redux";
 import Spinner from "../Spinner";
 
@@ -15,18 +15,16 @@ const Moves: React.FC = () => {
   const moves: IMove[] = pokemon.moves;
 
   return (
-    <div className={styles.moves}>
-      <center>
-        <h1>Top 3 moves</h1>
-      </center>
-      <div className={styles.container}>
+    <article className="info__container">
+      <h1>Top 3 moves</h1>
+      <div className="info__container-children">
         {moves.map((e, i) => {
           // eslint-disable-next-line array-callback-return
           if (i > 2) return;
           return <Move key={e.move.name} move={e.move} />;
         })}
       </div>
-    </div>
+    </article>
   );
 };
 
