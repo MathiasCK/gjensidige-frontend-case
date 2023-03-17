12763203b7f4f9e8966b9048c35f7d72b2176b7a
mathiasck <mathias.ck2000@gmail.com>
2023-03-17 09:29:03 +0100

    Card: code cleanup

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,19 +3,11 @@ import styles from "./Card.module.css";
 import Header from "../Header";
 import Moves from "../Moves";
 import Stats from "../Stats";
-import {useSelector} from "react-redux";
 
 const Card: React.FunctionComponent = () => {
-  const {pokemon} = useSelector((state: any) => state);
-
-  if (!pokemon || Object.keys(pokemon).length === 0) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div className={styles.container}>
       <Header />
-
       <Moves />
       <Stats />
     </div>
