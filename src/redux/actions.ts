4dfd42ad368f3ed26c6567fe08906eda0b5e0521
mathiasck <mathias.ck2000@gmail.com>
2023-03-17 09:13:08 +0100

    redux actions: fix typo

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -1,7 +1,7 @@
 import {Pokemon} from "../types";
 import {SET_POKEMON} from "./types";
 
-export const setPokemon = (obj: Pokemon) => ({
+export const setPokemonAction = (obj: Pokemon) => ({
   type: SET_POKEMON,
   payload: obj,
 });
