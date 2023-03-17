e3e015330efa46aa4d3781a169d207a0342fbe48
mathiasck <mathias.ck2000@gmail.com>
2023-03-17 11:13:28 +0100

    Card: css cleanup

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,16 +1,18 @@
 import React from "react";
-import styles from "./Card.module.css";
+import "./card.scss";
 import Header from "../Header";
 import Moves from "../Moves";
 import Stats from "../Stats";
 
 const Card: React.FunctionComponent = () => {
   return (
-    <div className={styles.container}>
-      <Header />
-      <Moves />
-      <Stats />
-    </div>
+    <section className="card__container">
+      <div className="card">
+        <Header />
+        <Moves />
+        <Stats />
+      </div>
+    </section>
   );
 };
 export default Card;
