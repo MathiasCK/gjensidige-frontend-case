897afb9cfcbb2c19260c4c16babdfd38d17e5f51
mathiasck <mathias.ck2000@gmail.com>
2023-03-19 21:47:02 +0100

    Persist redux state

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,17 @@
+import {loadState, saveState} from "@Utils/state";
 import {createStore} from "redux";
 import pokemonReducer from "./pokemon-reducer";
+// @ts-ignore
+import throttle from "lodash.throttle";
 
-const store = createStore(pokemonReducer);
+const persistedState = loadState();
+
+const store = createStore(pokemonReducer, persistedState);
+
+store.subscribe(
+  throttle(() => {
+    saveState(store.getState());
+  }, 1000),
+);
 
 export default store;
